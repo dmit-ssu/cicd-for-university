a8324bd1fee318fd71b1ca2e11cf5f39382c2662
devMauris <devmauris@gmail.com>
2015-02-26 13:35:43 +0300

    Installing plugins

--- install.sh
+++ install.sh
@@ -9,4 +9,15 @@ sudo cp -rp $DIRECTORY/common_jobs/* $HOME_JENKINS/jobs
 sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs/*
 sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs
 
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/greenballs.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/groovy-label-assignment.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/groovy.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/github.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git-client.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/github-api.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/git.hpi
+wget -P -N $HOME_JENKINS/plugins http://updates.jenkins-ci.org/latest/copyartifact.hpi
+sudo chown -R jenkins:jenkins $HOME_JENKINS/plugins/*
+
+
 sudo service jenkins start
