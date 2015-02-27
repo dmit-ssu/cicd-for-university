1fd9f9494f6f0d6fc638912ec0520933bc9a23c8
Kristina Polulyah <krismail95@gmail.com>
2015-02-27 14:28:58 +0300

    Fixing parametr script

--- slave_install.sh
+++ slave_install.sh
@@ -3,10 +3,18 @@
 ABSOLUTE_FILENAME=`readlink -e "$0"`
 DIRECTORY=`dirname "$ABSOLUTE_FILENAME"`
 
+HOST_MASTER = $1
+NAME_SLAVE = $2
+
 sudo mkdir /opt/cicdfuslave
+echo "echo topkek
+wget -O slave.jar http://$HOST_MASTER:8080/jnlpJars/slave.jar
+java -jar slave.jar -jnlpUrl http://$HOST_MASTER:8080//$NAME_SLAVE#_slave/slave-agent.jnlp &echo $! > /var/run/cicdfuslavejar.pid
+"
 sudo cp -rp $DIRECTORY/slaveservice/run.sh /opt/cicdfuslave
 sudo chmod +x /opt/cicdfuslave/run.sh
 
 sudo cp -rp $DIRECTORY/slaveservice/cicdfuslave /etc/init.d/
 sudo chmod +x /etc/init.d/cicdfuslave
 
+sudo service cicdfuslave start
